76475fc5183cba706f4f1ca3a83d146d6fd63e44
ivankukarin <ikukarin@yandex.ru>
2019-08-01 18:32:25 +0400

    without arrows

--- js/script.js
+++ js/script.js
@@ -1,61 +1,46 @@
-'use strict'
-let now = new Date();
-
-function checkData(data) {
-    let now = new Date();
-    let date = new Date(data);
-    let checkYear = (date.getFullYear() === now.getFullYear());
-    let checkMonths = (date.getMonth() === now.getMonth());
-    let checkDays = (date.getDate() - now.getDate());
-
-  return ((checkYear && checkMonths) && ( checkDays === 0)) ;
-    
-}
-
-function formatDate (data) {
-  let date = new Date(data);
-  function getMonths(date){
-    let i = date.getMonth();
-    const monthsArr = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
-    return monthsArr[i]
-  }
-  let formDate = (`${ date.getDate() } ${ getMonths(date) }`);
-  return formDate;
-}
-
-
-// 1564491000000
+"use strict";
 
 let weatherJul2019 = [
-    {
-      date: 1564491000000,
-      temperature: { night: 16, day: 26,},
-      cloudiness: 'Ясно',
-      snow: false,
-      rain: false,
+  {
+    date: 1564637264603,
+    temperature: { night: 16, day: 26 },
+    cloudiness: "Ясно",
+    snow: false,
+    rain: false
+  },
+  {
+    date: 1564723664603,
+    temperature: {
+      night: 19,
+      day: 29
     },
-    {
-      date: 1559505600000,
-      temperature: {
-        night: 19,
-        day: 29,
-      },
-      cloudiness: 'Облачно',
-      snow: false,
-      rain: true,
+    cloudiness: "Облачно",
+    snow: false,
+    rain: true
+  },
+  {
+    date: 1564810064603,
+    temperature: {
+      night: 12,
+      day: 21
     },
-    {
-      date: 1559592000000,
-      temperature: {
-        night: 12,
-        day: 21,
-      },
-      cloudiness: 'Облачно',
-      snow: false,
-      rain: false,
+    cloudiness: "Облачно",
+    snow: false,
+    rain: false
+  },
+  {
+    date: 1564896464603,
+    temperature: {
+      night: 12,
+      day: 21
     },
+    cloudiness: "Облачно",
+    snow: false,
+    rain: false
+  }
 ];
 
+let now = new Date();
 
 const imgClear = "/img/clear.png";
 const imgRain = "/img/rain.jpg";
@@ -63,24 +48,124 @@ const imgSnow = "/img/snow.jpg";
 const imgCloud = "/img/cloud.png";
 const imgRainSnow = "/img/rainSnow.jpg";
 
-let today = document.getElementsByClassName("today");
+function checkData(data, nextDay) {
+  let now = new Date();
+  let date = new Date(data);
+  let checkYear = date.getFullYear() === now.getFullYear();
+  let checkMonths = date.getMonth() === now.getMonth();
+  let checkDays = date.getDate() - now.getDate();
 
-let day = today[0].getElementsByClassName("day");
-let date = today[0].getElementsByClassName("date");
-let img = today[0].getElementsByTagName("img");
-let dayTemperature = today[0].getElementsByClassName("day-temperature");
-let nightTemperature = today[0].getElementsByClassName("night-temperature");
-let rainfallDescript = today[0].getElementsByClassName("rainfall-descript");
+  return checkYear && checkMonths && checkDays === nextDay;
+}
 
-console.log(img)
-// day.innerText = checkData(data)?"Сегодня":'Не сегодня';
-// date[0].innerHTML = formatDate(data);
-img
+// console.log(checkData(1564723664603, 1));
 
+function formatDate(data) {
+  let date = new Date(data);
+  function getMonths(date) {
+    let i = date.getMonth();
+    const monthsArr = [
+      "января",
+      "февраля",
+      "марта",
+      "апреля",
+      "мая",
+      "июня",
+      "июля",
+      "августа",
+      "сентября",
+      "октября",
+      "ноября",
+      "декабря"
+    ];
+    return monthsArr[i];
+  }
+  let formDate = `${date.getDate()} ${getMonths(date)}`;
+  return formDate;
+}
 
-// const refreshForecast = function (parentClass, currentClass, yourText) {
-//   let elem = document.querySelector(parentClass).querySelector(currentClass);
-//   elem.textContent = yourText;
-//   };
+function getDayOfWeek(anyDay) {
+  const arrDaysOfWeek = [
+    "воскресенье",
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота"
+  ];
+  let date = new Date(anyDay);
+  let numberDay = date.getDay();
+  return arrDaysOfWeek[numberDay];
+}
 
-// refreshForecast('today', 'night-temperature', 'Вот такие осадки');
+function getRainfallDescript(data) {
+  let cloudiness = data.cloudiness;
+  let rainfall = data => {
+    if (data.rain === true && data.snow === true) {
+      return "снег с дождем";
+    } else if (data.rain === true && data.snow === false) {
+      return "дождь";
+    } else if (data.rain === false && data.snow === true) {
+      return "снег";
+    } else if (data.rain === false && data.snow === false) {
+      return "без осадков";
+    }
+  };
+
+  let result = `${cloudiness}, \n ${rainfall(data)}`;
+  return result;
+}
+
+function rainfallImg(data) {
+  if (data.rain === true && data.snow === true) {
+    return imgRainSnow;
+  } else if (data.rain === true && data.snow === false) {
+    return imgRain;
+  } else if (data.rain === false && data.snow === true) {
+    return imgSnow;
+  } else if (data.rain === false && data.snow === false) {
+    if (data.cloudiness === "Облачно") {
+      return imgCloud;
+    } else {
+      return imgClear;
+    }
+  }
+}
+
+const today = document.querySelector(".today");
+const nextDay = document.querySelector(".next-day");
+const secondDay = document.querySelector(".second-day");
+const thirdDay = document.querySelector(".third-day");
+
+const arrDays = [today, nextDay, secondDay, thirdDay];
+
+// let index = array.find(x => checkData(x.date, 0));
+
+function applyWeather(array, index, currentDay) {
+  let day = currentDay.querySelector(".day");
+  let date = currentDay.querySelector(".date");
+  let img = currentDay.querySelector("img");
+  let dayTemperature = currentDay.querySelector(".day-temperature");
+  let nightTemperature = currentDay.querySelector(".night-temperature");
+  let rainfallDescript = currentDay.querySelector(".rainfall-descript");
+
+  day.innerText = checkData(array[index].date)
+    ? "Сегодня"
+    : getDayOfWeek(array[index].date);
+  date.innerText = formatDate(array[index].date);
+  img.setAttribute("src", rainfallImg(array[index]));
+  dayTemperature.innerText = `днем +${array[index].temperature.day}°`;
+  nightTemperature.innerText = `ночью +${array[index].temperature.night}°`;
+  rainfallDescript.innerText = getRainfallDescript(array[index]);
+}
+
+function setWeather(array) {
+  let index = array.findIndex(x => checkData(x.date, 0));
+  for (let i = 0; i <= 3; i++) {
+    applyWeather(array, index, arrDays[i]);
+    index++;
+  }
+}
+
+setWeather(weatherJul2019);
